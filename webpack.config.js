c17a9e9fdff99a4dc5c99bc053a51ab438b5d6e5
bxf_100_u <baixuexue123@hotmail.com>
2017-01-23 16:34:17 +0800

    change dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname,'app/main.js'),
-    output:{
-        path: path.resolve(__dirname,'build'),
-        filename: 'bundle.js'
-    }
+  entry: path.resolve(__dirname,'app/main.js'),
+  output:{
+    path: path.resolve(__dirname,'build'),
+    filename: 'bundle.js'
+  }
 
-    module:{
-        loaders: [
-            {
-                test: /\.json$/,
-                loader: "json"
-            },
+  module:{
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
 
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel', // 在webpack的module部分的loaders里进行配置即可
-            },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel', // 在webpack的module部分的loaders里进行配置即可
+      },
 
-            {
-                test: /\.css$/,
-                loader: 'style!css'// 添加对样式表的处理, 感叹号的作用在于使同一文件能够使用不同类型的loader
-            }
-        ]
-    },
+      {
+        test: /\.css$/,
+        loader: 'style!css'// 添加对样式表的处理, 感叹号的作用在于使同一文件能够使用不同类型的loader
+      }
+    ]
+  },
 
-    plugins: [
-        new webpack.BannerPlugin("Copyright Flying Unicorns inc.")//在这个数组中new一个就可以了
-    ],
+  plugins: [
+    new webpack.BannerPlugin("Copyright Flying Unicorns inc.")//在这个数组中new一个就可以了
+  ],
 
-    devServer: {
-        contentBase: "./public",
-        colors: true,
-        historyApiFallback: true,
-        inline: true
-    }
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
 };
